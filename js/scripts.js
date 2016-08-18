9a402b61dde3a9e9bb55a52ee0826b742a28ad6e
Jena SanCartier and Karen Freeman-Smith <jena@>
2016-08-18 09:47:00 -0700

    convert 500s to Ds

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,19 @@ $("form").submit(function(event){
 //Backend Logic...
 function romanize(aNumber){
   var romanNumber="";
+  var count=0;
   if (aNumber <= 3999) {
     for(var i=1;i<=parseInt(aNumber/1000);i++) {
       romanNumber+="M";
+      count=i;
     }
+    aNumber-=1000*count;
+    for(var i=1;i<=parseInt(aNumber/500);i++) {
+      romanNumber+="D";
+      count=i;
+    }
+    aNumber-=500*count;
+
   } else {
     romanNumber=("Sorry, maybe you should read the directions.");
   }
